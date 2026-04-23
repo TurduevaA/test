ae43f314aaec0652384e54afb62865e6e1c452f6
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2026-04-23 19:38:52 +0300

    create project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { styled } from "@mui/material";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Title>test</Title>
+    </div>
+  );
+};
+
+export default App;
+
+const Title = styled("h1")({
+  color: "red",
+});
