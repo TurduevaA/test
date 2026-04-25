0672320e2628ebd434eaf968fea7611c7e219a78
Adil <adiltop71@gmail.com>
2026-04-25 20:32:18 +0600

    Added registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { styled } from "@mui/material";
 import React from "react";
+import Registr from "./components/Registr";
 
 const App = () => {
   return (
     <div>
+      <Registr />
     </div>
   );
 };
